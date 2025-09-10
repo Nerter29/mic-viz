7488a46f302720a3d328c21df1bf59334bef49f3
Terence Chardes <terence.chardes@gmail.com>
2025-09-10 11:35:20 +0200

    smoothing of ras by adding an acceleration to every bars

--- js/logic.js
+++ js/logic.js
@@ -1,16 +1,18 @@
 
 
-export function extractSoudData(canvas, frequencyData, minHz, maxHz,logFactor, audioCtx, analyser, barNumber){
-    canvas.width= window.innerWidth - 150
-    canvas.height = window.innerHeight - 150
+export function extractSoudData(canvas, frequencyData, minHz, maxHz,logFactor, audioCtx, analyser,
+    barNumber){
+    canvas.width= window.innerWidth - 50
+    canvas.height = window.innerHeight - 80
 
     analyser.getByteFrequencyData(frequencyData);
 
-    let bars = []
+    let amplitudes = []
     const hzPerBin = audioCtx.sampleRate / analyser.fftSize;
+
     for (let i = 0; i < barNumber; i++) {
         const barProp = (i / barNumber);
-        const nextBarProp = ((i + 1) / barNumber); //proportions of the current bar based on total bars
+        const nextBarProp = ((i + 1) / barNumber); //proportions of the current bar based on total amplitudes
 
         //logarithmic and linear bin repartition.
         const f1Log = minHz * Math.pow(maxHz / minHz, barProp);
@@ -28,34 +30,35 @@ export function extractSoudData(canvas, frequencyData, minHz, maxHz,logFactor, a
         if (i1 < 0) i1 = 0;
         if (i2 >= frequencyData.length) i2 = frequencyData.length - 1;
 
-        //average Amplitude for each bars
+        //average Amplitude for each amplitudes
         let sum = 0;
         for (let j = i1; j <= i2; j++) {
             sum += frequencyData[j];
         }
-        sum /= i2 - i1
-        
-        bars.push(sum);
+        //another logarithm to highlight high Hz
+        sum /= (i2 -i1) - (0.025 * Math.pow(1000, i / barNumber))
+
+        amplitudes.push(sum);
     }
 
-    return bars
+    return amplitudes
 }
 
 
-export function updateCanvas(canvas, ctx, bars){
+export function updateCanvas(canvas, ctx, amplitudes, maxAmplitude, bars, barAcceleration){
     let cHeight = canvas.height
     let cWidth = canvas.width
-    let barsN = bars.length;
+    let amplitudesN = amplitudes.length;
     let separation = 2;
     if(window.innerWidth < 1000){
         separation = 1
     }
-    let barWidth = (cWidth - separation * (barsN * 2 + 1)) / (barsN * 2)
+    let barWidth = (cWidth - separation * (amplitudesN * 2 + 1)) / (amplitudesN * 2)
 
-    let maxAmplitude = 250
     ctx.clearRect( 0, 0, canvas.width, canvas.height);
     let x = separation
 
+    bars = updateBars(amplitudes, bars, barAcceleration)
     for(let j = 0; j < 2; j++){
         for(let i = 0; i < bars.length; i++){
             let k = i
@@ -66,12 +69,18 @@ export function updateCanvas(canvas, ctx, bars){
             let h = (bars[k] / maxAmplitude) * cHeight
 
             let y = cHeight - h
-            ctx.fillStyle = rainbowColors(bars[k] /maxAmplitude, k/ bars.length, 100, 250, 10, 50)
+            ctx.fillStyle = rainbowColors(bars[k] / maxAmplitude, k / bars.length, 100, 300, 5, 50)
             ctx.fillRect(x, y, w, h)
             x += barWidth + separation
 
         }
     }
+}
+function updateBars(amplitudes, bars, acceleration){
+    for(let i = 0; i < amplitudes.length; i++){
+        bars[i] += (amplitudes[i] - bars[i]) * acceleration
+    }
+    return bars
 }
 function rainbowColors(heightProportion, positionProportion, startH, endH, startIntensity, shininess){
     //get the color of a bar ased on its height and position
--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ import { extractSoudData, updateCanvas } from "./logic.js";
 const canvas = document.getElementById("barCanvas")
 const ctx = canvas.getContext('2d')
 
-canvas.width= window.innerWidth - 150
-canvas.height = window.innerHeight - 150
+canvas.width= window.innerWidth - 50
+canvas.height = window.innerHeight - 80
 
 const startButton = document.getElementById("startButton")
 startButton.addEventListener('click', ()=>{
@@ -15,20 +15,25 @@ startButton.addEventListener('click', ()=>{
 export async function startMic(canvas, ctx) {
   let barNumber = 24
   const minFreq = 50
-  const maxFreq = 10000;
-  const logFactor = 0.4
+  const maxFreq = 8000;
+  const logFactor = 0.5
+  const barAcceleration = 0.4
+  const maxAmplitude = 300
+
   const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
   const source = audioCtx.createMediaStreamSource(stream);
   const analyser = audioCtx.createAnalyser();
   analyser.fftSize = 2048;
 
+  let amplitudes =[]; //theorical values
+  let bars = new Array(barNumber).fill(0);; // graphical values
   source.connect(analyser);
   const frequencyData = new Uint8Array(analyser.frequencyBinCount);
   function update() {
 
-    let bars = extractSoudData(canvas, frequencyData, minFreq, maxFreq, logFactor, audioCtx, analyser, barNumber)
-    updateCanvas(canvas, ctx, bars)
+    amplitudes = extractSoudData(canvas, frequencyData, minFreq, maxFreq, logFactor, audioCtx, analyser, barNumber)
+    updateCanvas(canvas, ctx, amplitudes, maxAmplitude, bars, barAcceleration)
     requestAnimationFrame(update);
   }
 
